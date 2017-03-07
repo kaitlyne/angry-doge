1fca4d87cb623c642170ff07ba942e3b4d1ef1a8
Michael Li <michael.li11702@gmail.com>
2017-03-06 21:23:45 -0800

    Add more comments, align apply_gravity function

--- util-classes.js
+++ util-classes.js
@@ -7,14 +7,14 @@ class Moving_Ball {
     constructor(texture_filename, center_pos, radius, velocity=vec3(0,0,0)) {
         // material for ball's texture
         this.material = new Material(Color(0, 0, 0, 1), .7, .5, .0, 40, texture_filename);
-        // position of the ball's center
+        // position of the ball's center; this is a vec3
         this.center_pos = center_pos;
-        // ball's radius
+        // ball's radius; this should be constant
         this.radius = radius;
-        // ball's initial velocity
+        // ball's initial velocity; this is a vec3
         this.velocity = velocity;
 
-        // ball's model transform
+        // ball's model transform; this is a mat4
         this.transform = mat4();
         // set the initial transform to scale with the radius and translate to the initial position
         this.transform = mult(this.transform,
@@ -32,42 +32,48 @@ class Moving_Ball {
     // bounce_factor specifies how much the velocity is multiplied by when bouncing back up
     // from the ground
     apply_gravity(frame_delta, gravity_const, bounce_factor) {
-              // change in velocity is acceleration * change in time
-              // acceleration in this case is just a constant that looks good
-              this.velocity[1] -= frame_delta * gravity_const;
-              // set displacement to be velocity
-              var displacement = this.velocity;
-              // calculate new position with given velocity
-              var new_pos = add(this.center_pos, this.velocity);
-              // if sphere's bottom is below the floor
-              if (new_pos[1] - this.radius < FLOOR_Y_POS) {
-                  const bounce_factor = 0.85;
-                  // flip the ball's y velocity to make it bounce, multiply by bounce factor
-                  this.velocity[1] *= -bounce_factor;
-                  // don't want the ball to appear partly "inside" the floor, so adjust the displacement
-                  // so that it's right above the floor
-                  //new_pos = add(this.center_pos, this.velocity);
-                  new_pos = add(vec3(this.center_pos[0],
-                              FLOOR_Y_POS + this.radius, this.center_pos[2]),
-                              this.velocity);
-                  displacement = subtract(new_pos, this.center_pos);
-                  //console.log(this.center_pos[1], new_pos[1], this.velocity);
-              }
-              this.center_pos = new_pos;
-              // translate the ball by the displacement
-              this.transform = mult(translation(displacement[0],
-                          displacement[1], displacement[2]),
-                          this.transform);
+        // change in velocity is acceleration * change in time
+        // acceleration in this case is just a constant that looks good
+        this.velocity[1] -= frame_delta * gravity_const;
+        // set displacement to be velocity
+        var displacement = this.velocity;
+        // calculate new position with given velocity
+        var new_pos = add(this.center_pos, this.velocity);
+        // if sphere's bottom is below the floor
+        if (new_pos[1] - this.radius < FLOOR_Y_POS) {
+            const bounce_factor = 0.85;
+            // flip the ball's y velocity to make it bounce, multiply by bounce factor
+            this.velocity[1] *= -bounce_factor;
+            // don't want the ball to appear partly "inside" the floor, so adjust the displacement
+            // so that it's right above the floor
+            //new_pos = add(this.center_pos, this.velocity);
+            new_pos = add(vec3(this.center_pos[0],
+                        FLOOR_Y_POS + this.radius, this.center_pos[2]),
+                    this.velocity);
+            displacement = subtract(new_pos, this.center_pos);
+            //console.log(this.center_pos[1], new_pos[1], this.velocity);
+        }
+        this.center_pos = new_pos;
+        // translate the ball by the displacement
+        this.transform = mult(translation(displacement[0],
+                    displacement[1], displacement[2]),
+                this.transform);
     }
 }
 
 function do_balls_collide(ball1, ball2) {
+    // get the difference between the two balls' centers as a vector
     var position_difference = subtract(ball1.center_pos, ball2.center_pos);
-    //var distance_squared = position_difference[0] * position_difference[0] +
-    //    position_difference[1] * position_difference[1] +
-    //    position_difference[2] * position_difference[2];
+    // calculate the length of this vector squared; don't need the length
+    // itself as square root is expensive and unnecessary
+    // the length squared is the same as the vector dotted with itself:
+    // x*x + y*y + z*z
     var distance_squared = dot(position_difference, position_difference);
+    // calculate (sum of the balls' radii)^2
     var radii_sum_squared = Math.pow(ball1.radius + ball2.radius, 2);
+    // if squared distance is less than squared sum of radii,
+    // that's the same as saying the distance is less than the sum of the radii
+    // aka the balls collided
     if (distance_squared < radii_sum_squared) {
         return true;
     }
